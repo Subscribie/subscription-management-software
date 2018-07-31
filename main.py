c163371b2b040ed6d67fdef7719ee77e3917c6b5
chrisjsimpson <chris@karmacomputing.co.uk>
2018-07-31 14:12:39 +0100

    initial Partner data model

--- main.py
+++ main.py
@@ -32,6 +32,11 @@ class TransactionGatewayAbstract:
 
     @abstractmethod
     def get_short_name(self):
+        '''Must be unique, we don't have a registry for this, obviously, 
+        but it makes sense to provide Organisationally Unique Identifier (OUI)'s
+        for this to allow systems to distinguish uniqe records accross systems
+        whilst still retaining their origin (different from using a uuid, which
+        would provide uniqueness, but not reference).'''
         raise NotImplementedError()
 
     @abstractmethod
@@ -51,6 +56,60 @@ Transaction = namedtuple('Transaction', ['date', 'amount', 'reference',
                                          'source_type', 'source_id'])
 
 Transaction.__new__.__defaults__ = (None,) * len(Transaction._fields)
+
+Partner = namedtuple('Partner', ['uid', 
+                                 'created_at',
+                                 'matched_source_gateways',
+                                 'links_partner_uids',
+                                 'language',
+                                 'billing_email',
+                                 'given_name',
+                                 'family_name',
+                                 'company_name',
+                                 'billing_street_number',
+                                 'billing_street',
+                                 'billing_city',
+                                 'billing_postal_code',
+                                 'billing_state',
+                                 'billing_state_code',
+                                 'billing_country',
+                                 'billing_country_code',
+                                 'billing_latitude',
+                                 'billing_longitude'
+                                 'shipping_street_number',
+                                 'shipping_street',
+                                 'shipping_city',
+                                 'shipping_country',
+                                 'shipping_country_code',
+                                 'shipping_latitude',
+                                 'shipping_longitude',
+                                 'shipping_postal_code',
+                                 'shipping_state',
+                                 'shipping_statecode',
+                                 'active_mandates',
+                                 ])
+Partner.__new__.__defaults__ = (None,) * len(Partner._fields)
+
+'''
+ How to decide if a partner is already matched in matched_source_gateways.
+ We use the word 'partner' instead of 'customer' to normalise these types of
+ records, inspired by Odoo's data model, whereas Salesforce uses the word
+ 'account' for the same purpose.
+
+ 1) Take a customer object from a TransactionGatewayAbstract
+ 2) Extract the unique id which the TransactionGateway provides for the partner 
+ 3) Generate a look-up id (aka TransactionGatewayPartnerId) by concatenating the 
+    TransactionGatewayAbstract.get_short_name with the unique partner id
+    provided by the TransactionGateway being inspected. (We loosely take
+    inspiration from MAC Organisationally Unique Identifier (OUI) concept to
+    ensure uniqueness.
+ 4) Query the Partner object for the computed TransactionGatewayPartnerId
+ 5) If the TransactionGatewayPartnerId exists within a Partner's 
+    matched_source_gateways attribute, then this record corresponds to that
+    gateway. If not, then this is considered a new 'unseen' Partner records
+    which MAY have data which can be used to update the Partner record.
+'''
+
 
 class SSOT:
     """ Single Source Of Truth (SSOT) transaction gateway.
