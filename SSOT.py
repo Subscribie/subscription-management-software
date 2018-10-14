8185066e690ce1a18b23d854b50443a7cf61bf85
chrisjsimpson <chris@karmacomputing.co.uk>
2018-10-14 01:37:34 +0100

    Refactored SSOT so that it is easier for client code to choose which gateways to dynamically load at runtime. target gateways can be passed into SSOT's constructor: target_gateways = ({'name': 'GoCardless', 'construct': access_token},) each target gateway may pass  as kwargs should the target gateway constructor require them. The target_gateway name must be the name of the module to load in, and it is assumed the target gateway class has the same name as the module

--- SSOT.py
+++ SSOT.py
@@ -8,7 +8,9 @@ class SSOT(TransactionGatewayAbstract, PartnerGatewayAbstract):
     Merges other gateway transactions, and partner gateways
     """
 
-    def __init__(self):
+    def __init__(self, target_gateways=None):
+        self.target_gateways = target_gateways
+        self.loaded_gateways = []
         self.transactions = []
         self.partners = []
         self.init()
@@ -22,19 +24,49 @@ class SSOT(TransactionGatewayAbstract, PartnerGatewayAbstract):
         return "SSOT"
 
     def init(self):
-        self.HSBC = HSBCBusiness()                                                            
-        self.HSBC.fetchTransactions()
-        self.HSBC.fetchPartners()
-        self.GC = GoCardless()
-        self.GC.fetchTransactions()
-        self.GC.fetchPartners()
+        if self.target_gateways is None:
+            self.HSBC = HSBCBusiness()
+            self.HSBC.fetchTransactions()
+            self.HSBC.fetchPartners()
+            self.loaded_gateways.append('HSBC')
+            self.GC = GoCardless()
+            self.GC.fetchTransactions()
+            self.GC.fetchPartners()
+            self.loaded_gateways.append('GC')
+        else:
+            for gateway in self.target_gateways:
+                gatewayName = gateway['name']
+                gatewayModule = __import__(gatewayName)
+                gatewayClass = getattr(gatewayModule,gatewayName)
+                shortName = gatewayClass.get_short_name()
+                gatewayInstance = gatewayClass(gateway['construct'])
+                gatewayInstance.fetchTransactions()
+                gatewayInstance.fetchPartners()
+                # Dynamically runs: from <gatewayName> import <gatewayClass>
+                # and than adds <gatewayClass> instance as property to SSOT
+                setattr(self, shortName, gatewayInstance)
+                self.loaded_gateways.append(shortName)
+
+        # Finally Join SSOT Partners & Transactions from all loaded gateways
+        self.fetchTransactions()
+        self.fetchPartners()
 
     def fetchTransactions(self):
-        self.transactions = self.HSBC.transactions + self.GC.transactions
+        if self.target_gateways is None:
+            self.transactions = self.HSBC.transactions + self.GC.transactions
+        else:
+            for gateway in self.loaded_gateways:
+                gatewayTransactions = self.__getattribute__(gateway).transactions
+                self.transactions = self.transactions + gatewayTransactions
 
     def fetchPartners(self):
-        self.partners = self.HSBC.partners + self.GC.partners
-
+        if self.target_gateways is None:
+            self.partners = self.HSBC.partners + self.GC.partners
+        else:
+            for gateway in self.loaded_gateways:
+                gatewayPartners = self.__getattribute__(gateway).partners
+                self.partners = self.partners + gatewayPartners
+        
     def filterby(self, source_gateway=None, source_id=None, reference=None, *args, **kwargs):
         if source_gateway:
             try:
